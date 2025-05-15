c792ceaa04ebc4c480ce220f811df1ef66dcf024
tomiella <tom@pranjic.at>
2025-05-15 16:49:25 +0200

    Changes by kokskorcian

---
+++ src/main/java/at/bif/swen/tourplanner/model/Routelog.java
@@ -0,0 +1,69 @@
+package at.bif.swen.tourplanner.model;
+
+import java.util.Date;
+
+public class Routelog {
+    private Date datetime;
+    private String comment;
+    private int difficulty;
+    private int rating;
+    private int duration;
+    private RouteItem route;
+
+    public Routelog(Date datetime, String comment, int difficulty, int rating, int duration, RouteItem route) {
+        this.datetime = datetime;
+        this.comment = comment;
+        this.difficulty = difficulty;
+        this.rating = rating;
+        this.duration = duration;
+        this.route = route;
+    }
+
+    public Date getDatetime() {
+        return datetime;
+    }
+
+    public void setDatetime(Date datetime) {
+        this.datetime = datetime;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(int difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public RouteItem getRoute() {
+        return route;
+    }
+
+    public void setRoute(RouteItem route) {
+        this.route = route;
+    }
+}
