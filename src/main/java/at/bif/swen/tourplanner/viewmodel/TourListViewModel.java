fa65f8313216e5b950ad345fde52932016e61a3b
tomiella <tom@pranjic.at>
2025-05-20 16:40:59 +0200

    added tour add dialog

--- src/main/java/at/bif/swen/tourplanner/viewmodel/TourListViewModel.java
+++ src/main/java/at/bif/swen/tourplanner/viewmodel/TourListViewModel.java
@@ -6,9 +6,14 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
 
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
+import java.util.Optional;
 
 public class TourListViewModel {
     private final TourManager tourManager;
@@ -28,7 +33,50 @@ public class TourListViewModel {
     }
 
     public void addTour() {
-        // TODO: add tours
+        Dialog<TourItem> dialog = new Dialog<>();
+        dialog.setTitle("Add Tour");
+        dialog.setHeaderText("Add a new tour");
+
+        ButtonType addType = new ButtonType("Add", ButtonBar.ButtonData.OK_DONE);
+        dialog.getDialogPane().getButtonTypes().addAll(addType, ButtonType.CANCEL);
+
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(20, 150, 10, 10));
+
+        TextField nameField = new TextField();
+        nameField.setPromptText("Tour name");
+        TextField descField = new TextField();
+        descField.setPromptText("Description");
+
+        grid.add(new Label("Name:"),    0, 0);
+        grid.add(nameField,            1, 0);
+        grid.add(new Label("Description:"), 0, 1);
+        grid.add(descField,            1, 1);
+
+        dialog.getDialogPane().setContent(grid);
+
+        Node addButtonNode = dialog.getDialogPane().lookupButton(addType);
+        addButtonNode.setDisable(true);
+        nameField.textProperty().addListener((obs, oldVal, newVal) ->
+                addButtonNode.setDisable(newVal.trim().isEmpty())
+        );
+
+        dialog.setResultConverter(dialogButton -> {
+            if (dialogButton == addType) {
+                TourItem newTour = new TourItem(nameField.getText());
+                newTour.setDescription(descField.getText());
+                return newTour;
+            }
+            return null;
+        });
+
+        Optional<TourItem> result = dialog.showAndWait();
+
+        result.ifPresent(tour -> {
+            tourManager.createTour(tour.getName());
+        });
     }
 
     public void searchTours() {
