2e3b9ac741c79eb60ba09cac9b9e70d579a083ed
tomiella <tom@pranjic.at>
2025-05-21 12:02:06 +0200

    fix responsiveness

--- src/main/java/at/bif/swen/tourplanner/TourPlannerApplication.java
+++ src/main/java/at/bif/swen/tourplanner/TourPlannerApplication.java
@@ -62,7 +62,7 @@ public class TourPlannerApplication extends Application {
 
     public static void showStage(Stage stage, Parent root) {
         Scene scene = new Scene(root);
-        stage.setMinWidth(500);
+        stage.setMinWidth(610);
         stage.setMinHeight(400);
         stage.setTitle("TourPlanner");
         stage.sizeToScene();
